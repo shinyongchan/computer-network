4c3667c287648b7da1a84119bb192d0bf6e50e5b
syc <tlsdydcks135@naver.com>
2023-02-14 21:22:56 +0900

    Day22Mision02

--- Day22Mission.java
+++ Day22Mission.java
@@ -1,37 +1,34 @@
-//Mission 1
-class Worker extends Thread{
-    public void run(){
-        for (int i = 0; i <= 4; i++){
-            System.out.println("작업 스레드 : " + i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e){
+//Mission 2
 
-            }
-        }
-
-    }
-}
 
 
 public class Day22Mission {
     public static void main(String args[]) {
-        Thread thread = new Worker();
-        thread.start();
+          Thread worker = new Thread(()->{
+          for (int i = 0; i <= 4; i++){
+              System.out.println("작업 스레드 : " + i);
+              try {
+                  Thread.sleep(1000);
+              } catch (InterruptedException e){
 
-        int i = 0;
-        int alphabet = 'a';
-        while (true){
-            if (!thread.isAlive())
-                break;
-            System.out.println("메인 스레드 : " + (char)(alphabet+i));
-            i ++;
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e){
-
+              }
             }
-        }
+          });
+
+          worker.start();
+          int i = 0;
+          int alphabet = 'a';
+          while (true){
+              if (!worker.isAlive())
+                  break;
+              System.out.println("메인 스레드 : " + (char)(alphabet+i));
+              i ++;
+              try {
+                  Thread.sleep(500);
+              } catch (InterruptedException e){
+
+              }
+         }
 
     }
 }
