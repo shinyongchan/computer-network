c622091347f9c19078d8bfb8e8922cf7aeb881b8
syc <tlsdydcks135@naver.com>
2023-02-15 21:54:13 +0900

    Day23Mission02

--- Day23Mission.java
+++ Day23Mission.java
@@ -1,21 +1,20 @@
-// Day23Mission01
+// Day23Mission02
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day23Mission {
     public static void main(String[] args) {
-        List <String> names = List.of("홍길동", "배장화", "임꺽정", "연흥부", "김선달", "황진이");
-        Stream <String> n = names.stream();
-        n.filter(s -> s.charAt(0) < '이').forEach(s -> System.out.print(s+" "));
-        System.out.println();
-        n = names.stream();
-        n.sorted().forEach(s -> System.out.print(s+" "));
-        System.out.println();
-        n = names.stream();
-        System.out.println(n.findFirst());
-        n = names.stream();
-        System.out.println(n.findFirst().get());
-        n = names.stream();
-        System.out.println(n.count());
+
+        List<String> names = List.of("홍길동", "배장화", "임꺽정", "연흥부", "김선달", "황진이");
+        List<Integer> ages = List.of(25, 20, 29, 28, 32, 18);
+        List<Gender> genders = List.of(Gender.남, Gender.여, Gender.남, Gender.남, Gender.남, Gender.여);
+        Stream<Integer> a = ages.stream();
+        System.out.println(a.reduce((x, y) -> x + y).get());
+        a = ages.stream();
+        System.out.println(a.max((x, y) -> x - y).get());
+        IntStream ai = ages.stream().mapToInt(i -> i.intValue());
+        System.out.println(ai.average().getAsDouble());
     }
 }
+enum Gender {남, 여};
